4f3238845ddcc49cab0d3c49ae5086d4242b354a
David E Melnikoff <davidemelnikoff@gmail.com>
2023-11-05 22:38:11 -0800

    @deploy

--- src/experiment.js
+++ src/experiment.js
@@ -60,8 +60,8 @@ const MakeFlowQs = function(order) {
         {prompt: `During the ${order} version of the typing task, how <strong>engrossed</strong> did you feel in what you were doing?`,
         name: `engrossed_${order}`,
         labels: ["0<br>Not very engrossed", '1', '2', '3', '4', '5', '6', '7', '8', '9', "10<br>More engrossed than I've ever felt"]},
-        {prompt: `During the ${order} version of the typing task, how often did your attention drift away from the task at hand?`,
-        name: `drift_${order}`,
+        {prompt: `During the ${order} version of the typing task, how often did you find yourself wondering when the task would end?`,
+        name: `wonder_${order}`,
         labels: ["0<br>Never", '1', '2', '3', '4', '5', '6', '7', '8', '9', "10<br>Constantly"]},
     ];
     this.randomize_question_order = false;
